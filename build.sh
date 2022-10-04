0d3b7dad6f99f1a9784a776d98db4477c85ad603
Mikhail Petrochenkov <mishok2503@mail.ru>
2022-10-04 18:59:13 +0300

    add server

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 set -x
 
 export PROTOS_PATH=${PROTOS_PATH="src/common/protos/"}
+export PLAYER_PATH=${PLAYER_PATH="src/player/server/"}
 
-pushd $PROTOS_PATH
-python -m grpc_tools.protoc -I . player.proto --python_out=. --grpc_python_out=.
-popd
+python -m grpc_tools.protoc -I $PROTOS_PATH --python_out=$PLAYER_PATH --grpc_python_out=$PLAYER_PATH player.proto
---
+++ src/player/server/server.py
@@ -0,0 +1,26 @@
+from concurrent.futures import ThreadPoolExecutor
+
+import grpc
+from player_pb2 import PlayerVersion
+from player_pb2_grpc import PlayerBackendServicer, add_PlayerBackendServicer_to_server
+
+
+class Service(PlayerBackendServicer):
+    """GRPC player backend."""
+
+    def get_player_version(self, request, context):
+        """Return default player version."""
+        return PlayerVersion(1, 0)
+
+
+def execute_server():
+    """Run gRPC server."""
+    server = grpc.server(ThreadPoolExecutor(max_workers=10))
+    add_PlayerBackendServicer_to_server(Service(), server)
+    server.add_insecure_port("[::]:3000")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    execute_server()
